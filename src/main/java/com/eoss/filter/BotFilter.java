33f780bdcfc690ef4fcc75b74d5ee267a4c689c3
Noom <chalat@eoss-th.com>
2018-04-26 10:40:03 +0700

    backup

--- src/main/java/com/eoss/filter/BotFilter.java
+++ src/main/java/com/eoss/filter/BotFilter.java
@@ -8,12 +8,26 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletRequestWrapper;
 
 public class BotFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		if (!req.isSecure()) {
+			String uri = "https://" +   // "http" + "://
+		             req.getServerName() +       // "myhost"
+		             req.getRequestURI() +       // "/people"
+		            (req.getQueryString() != null ? "?" +
+		             req.getQueryString() : ""); // "?" + "lastname=Fox&age=30"
+			
+			resp.sendRedirect(uri);
+			return;
+		}
 		
 		HttpServletRequestWrapper requestWrapper = new HttpServletRequestWrapper((HttpServletRequest) request) {
 
