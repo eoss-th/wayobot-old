51b32872e9329b20c5cc09261cf301b6c8b5fe6e
Noom <chalat@eoss-th.com>
2018-04-27 10:15:46 +0700

    backup

--- src/main/java/com/eoss/filter/BotFilter.java
+++ src/main/java/com/eoss/filter/BotFilter.java
@@ -16,7 +16,7 @@ public class BotFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		
-		/*HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse resp = (HttpServletResponse) response;
 		if (!req.isSecure()) {
 			String uri = "https://" +   // "http" + "://
@@ -27,7 +27,7 @@ public class BotFilter implements Filter {
 			
 			resp.sendRedirect(uri);
 			return;
-		}*/
+		}
 		
 		HttpServletRequestWrapper requestWrapper = new HttpServletRequestWrapper((HttpServletRequest) request) {
 
---
+++ src/main/java/com/eoss/filter/DomainFilter.java
@@ -0,0 +1,46 @@
+package com.eoss.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+/*
+public class DomainFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		String origin = req.getHeader("");
+		
+		if (origin==null || !origin.equals(domain)) {
+			resp.sendRedirect(domain);
+			return;
+		}
+		
+		chain.doFilter(request, response);	
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+*/
