ba68565787ab6c616a0db6314ce2494cd84a20ae
Noom <chalat@eoss-th.com>
2018-04-28 22:37:39 +0700

    backup

--- src/main/java/com/eoss/appengine/service/v0_001/ShowCaseService.java
+++ src/main/java/com/eoss/appengine/service/v0_001/ShowCaseService.java
@@ -1,7 +1,10 @@
 package com.eoss.appengine.service.v0_001;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.TimeZone;
 
 import javax.servlet.ServletException;
@@ -9,10 +12,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.eoss.appengine.bean.ChatRoom;
 import com.eoss.appengine.bean.ShowCase;
 import com.eoss.appengine.bean.ShowcaseBotRoom;
+import com.eoss.appengine.dao.ChatRoomDAO;
 import com.eoss.appengine.dao.ShowCaseDAO;
 import com.eoss.appengine.dao.ShowcaseBotRoomDAO;
+import com.eoss.appengine.helper.SetReqParam;
 import com.eoss.appengine.helper.SetRespPram;
 import com.google.appengine.api.datastore.Entity;
 
@@ -20,6 +26,7 @@ import com.google.appengine.api.datastore.Entity;
 @SuppressWarnings("serial")
 public class ShowCaseService extends HttpServlet{
 	private SetRespPram srp = new SetRespPram();
+	private SetReqParam srq = new SetReqParam();
 	private ShowCase showcase = new ShowCase();
 	private ShowCaseDAO showcaseDao = new ShowCaseDAO();
 
@@ -87,7 +94,29 @@ public class ShowCaseService extends HttpServlet{
 						// remove showcase from all chat room
 						ShowcaseBotRoomDAO showcaseBotRoomDao = new ShowcaseBotRoomDAO();
 						Entity showcaseBotRoomEnt = showcaseBotRoomDao.getShowcaseBotRoom(botId);
+						List<String> roomListArray = new ArrayList<String>();
 						if(showcaseBotRoomEnt != null) {
+							if(showcaseBotRoomEnt.getProperty("roomList") != null) {
+								String  roomList = showcaseBotRoomEnt.getProperty("roomList").toString();
+								roomListArray = new ArrayList<String>(Arrays.asList(roomList.replace("[", "").replace("]", "").replaceAll(" ", "").split(",")));
+							}
+							ChatRoomDAO chatRoomDao = new ChatRoomDAO();
+							List<String> contextList = new ArrayList<String>();
+							for(String room:roomListArray) {
+								Entity chatRoomEnt = chatRoomDao.getChatRoom(room);
+								if(chatRoomEnt != null) {
+									if (chatRoomEnt.getProperty("botPath") != null) {
+										String botPathList = chatRoomEnt.getProperty("botPath").toString();
+										contextList = new ArrayList<String>(Arrays
+												.asList(botPathList.replace("[", "").replace("]", "").replaceAll(" ", "").split(",")));
+									}
+									if (contextList.size() > 0 && contextList.contains(accountId+"/"+botId)) {
+										contextList.remove(accountId+"/"+botId);
+										chatRoomEnt.setIndexedProperty("botPath", contextList);
+										srq.datasotorePut(chatRoomEnt);
+									}
+								}
+							}
 							
 							showcaseBotRoomDao.delShowcaseBotRoom(botId);
 						}
